194e9b4d6344a10a7b385260d7c2f626205fd065
GitHub Action <action@github.com>
2025-06-15 10:24:26 +0000

    bump: Update version to 0.1.5

--- ui_zero/cli.py
+++ ui_zero/cli.py
@@ -550,7 +550,7 @@ def main() -> None:
         "--command", "-c", action="append", help=get_text("arg_command_help")
     )
 
-    parser.add_argument("--version", "-v", action="version", version="UI-Zero v0.1.4")
+    parser.add_argument("--version", "-v", action="version", version="UI-Zero v0.1.5")
 
     parser.add_argument(
         "--no-history", action="store_true", help=get_text("arg_no_history_help")
